0e22f5f9a9e81d43a00613e878981c7b30b96862
LR32768 <lr200898@126.com>
2019-01-01 17:54:36 +0800

    Add files via upload

--- maths.h
+++ maths.h
@@ -4,8 +4,7 @@
  */
 #ifndef MATHS_H_
 #define MATHS_H_
-#include <cmath>
-#define INF 1e10
+#define INF 1e20
 #define EPS 1e-4
 
 struct Vec {
@@ -14,7 +13,7 @@ struct Vec {
 	Vec operator+ (const Vec& b) const { return Vec(x+b.x, y+b.y, z+b.z); }
 	Vec operator- (const Vec& b) const { return Vec(x-b.x, y-b.y, z-b.z); }
 	Vec operator* (const double b) const { return Vec(x*b, y*b, z*b); }
-	Vec	operator* (const Vec& b) const { return Vec(x*b.x, y*b.y, z*b.z); }
+	Vec	mult(const Vec& b) const { return Vec(x*b.x, y*b.y, z*b.z); }
 	Vec& norm() { return *this = *this * (1 / sqrt(x*x + y*y + z*z)); }
 	//dot product
 	double dot(const Vec& b) const {return x*b.x + y*b.y + z*b.z; } 
@@ -38,12 +37,12 @@ struct Ray {
 
 static unsigned long rx = 123456789, ry = 362436069, rz = 521288629;
 unsigned long frand(void) 
-///A fast random generator based on LFSR with period 2^96-1
+	///A fast random generator based on LFSR with period 2^96-1
 {	
 	unsigned long t;
-    rx ^= rx << 16;
-    rx ^= rx >> 5;
-    rx ^= rx << 1;
+	rx ^= rx << 16;
+	rx ^= rx >> 5;
+	rx ^= rx << 1;
 	t = rx;
 	rx = ry;
 	ry = rz;
@@ -53,8 +52,8 @@ unsigned long frand(void)
 
 double rand01(void)
 {
-	unsigned long MAX = (1 << 32);
-	return (frand() % MAX) / MAX;
+	unsigned long MAX = (1 << 20);
+	return double (frand() % MAX) / double(MAX);
 }
 
 //clamp function, clamp every double into [0, 1]
@@ -62,5 +61,5 @@ inline double clamp(double x){ return x<0 ? 0 : x>1 ? 1 : x; }
 
 //round every colour channel lightness into 0-255 value
 inline int toInt(double x){ return int(pow(clamp(x),1/2.2)*255+.5); }
-
+ 
 #endif
